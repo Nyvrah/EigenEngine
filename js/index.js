aad1fffabf74c99df054b3f0a9bfe147ee25c69b
Harvyn Lim <harvyn.lim@duke.edu>
2024-10-02 19:18:49 -0400

    1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let max = 10;
+
 MathJax = {
     tex: {inlineMath: [['$','$']]}
 }
@@ -23,8 +25,14 @@ $('.generate-button').click(function(){
         case "mat-vec-pro":
             type = matVecPro;
             break;
+        case "mat-mul":
+            type = matMul;
+            break;
+        case "rref":
+            type = rref;
+            break;
         default:
-            type=null;
+            type = null;
             break;
     }
     if (type){
@@ -43,18 +51,17 @@ function createProblems(type,amount){
             $('#s'+n).css("background-color","transparent");
             $('#s'+n+':hover').css("transform","none");
             var questions = document.querySelectorAll('.solution');
-            MathJax.typesetPromise(questions)
+            MathJax.typesetPromise(questions);
         });
     }
     var questions = document.querySelectorAll('.question');
     MathJax.typesetPromise(questions)
-    .catch((err) => console.error('Error typesetting MathJax:', err));
 }
 
 function vecAdd(){
     var x = getRand(3,6);
-    const vec1 = getMat(x,1,1,30);
-    const vec2 = getMat(x,1,1,30);
+    const vec1 = getMat(x,1,-max,max);
+    const vec2 = getMat(x,1,-max,max);
     const sol = [];
     for (let i=0;i<x;i++){
         sol.push([parseInt(vec1[i])+parseInt(vec2[i])]);
@@ -65,8 +72,8 @@ function vecAdd(){
 function matVecPro(){
     var x = getRand(3,6);
     var y = getRand(3,6);
-    const mat = getMat(x,y,1,6);
-    const vec = getMat(y,1,1,6);
+    const mat = getMat(x,y,-max,max);
+    const vec = getMat(y,1,-max,max);
     const sol = [];
     for (let i=0;i<x;i++){
         var n = 0;
@@ -77,6 +84,78 @@ function matVecPro(){
         sol.push([n]);
     }
     return ["$" + toStr(mat) + toStr(vec) + "=$", "$" + toStr(sol) + "$"];
+}
+
+function matMul(){
+    var x = getRand(3,6);
+    var y = getRand(3,6);
+    var z = getRand(3,6);
+    const mat1 = getMat(x,y,-max,max);
+    const mat2 = getMat(y,z,-max,max);
+    const sol = [];
+    for (let i=0;i<x;i++){
+        var n = []
+        for (let k=0;k<z;k++){
+            var m = 0;
+            for (let j=0;j<y;j++){
+                m += parseInt(mat1[i][j]) * parseInt(mat2[j][k]);
+            }
+            n.push(m);
+        }
+        sol.push(n);
+    }
+    return ["$" + toStr(mat1) + toStr(mat2) + "=$", "$" + toStr(sol) + "$"];
+}
+
+function rref(){
+    var x = getRand(3,6);
+    var y = getRand(x,6);
+    const mat = getMat(x,y,-max,max);
+    console.log(mat);
+
+    const matrix = [];
+    for (var i = 0; i < mat.length; i++){
+        matrix[i] = mat[i].slice();
+    }
+        
+    let lead = 0;
+
+    for (let r = 0; r < matrix.length; r++) {
+      if (lead >= matrix[0].length) {
+        return matrix;
+      }
+  
+      let i = r;
+      while (matrix[i][lead] === 0) {
+        i++;
+        if (i === matrix.length) {
+          i = r;
+          lead++;
+          if (lead === matrix[0].length) {
+            return matrix;
+          }
+        }
+      }
+  
+      [matrix[i], matrix[r]] = [matrix[r], matrix[i]];
+  
+      let val = matrix[r][lead];
+      for (let j = 0; j < matrix[0].length; j++) {
+        matrix[r][j] /= val;
+      }
+  
+      for (let i = 0; i < matrix.length; i++) {
+        if (i !== r) {
+          val = matrix[i][lead];
+          for (let j = 0; j < matrix[0].length; j++) {
+            matrix[i][j] -= val * matrix[r][j];
+          }
+        }
+      }
+      lead++;
+    }
+
+    return ["$rref" + toStr(mat) + "=$",  "$" + toStr(matrix) + "$"];
 }
 
 function toStr(mat){
