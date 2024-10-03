414c16b927c34915cc648cab9d81e11d1d4fc79b
Harvyn Lim <harvyn.lim@duke.edu>
2024-10-03 11:35:35 -0400

    1

--- js/index.js
+++ js/index.js
@@ -155,6 +155,12 @@ function rref(){
       lead++;
     }
 
+    for (let i=0;i<matrix.length;i++){
+        for (let j=0;j<matrix[0].length;j++){
+            matrix[i][j] = Math.round(matrix[i][j] * 100) / 100
+        }
+    }
+
     return ["$rref" + toStr(mat) + "=$",  "$" + toStr(matrix) + "$"];
 }
 
